53a02bf0236363a6a7f53e73ab88c185c374b31c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-11-18 17:18:36 -0800

    extra 6

---
+++ src/exercise/06.extra-6.js
@@ -0,0 +1,85 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.extra-6.js
+
+import * as React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
+
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon'
+
+function PokemonInfo({pokemonName}) {
+  const [state, setState] = React.useState({
+    status: 'idle',
+  })
+
+  React.useEffect(() => {
+    if (!pokemonName) return
+
+    setState({status: 'pending'})
+    fetchPokemon(pokemonName)
+      .then(pokemonData => {
+        setState({status: 'resolved', pokemon: pokemonData})
+      })
+      .catch(error => {
+        setState({status: 'rejected', error})
+      })
+  }, [pokemonName])
+
+  const isIdle = state.status === 'idle'
+  const isPending = state.status === 'pending'
+  const isRejected = state.status === 'rejected'
+  const isResolved = state.status === 'resolved'
+
+  if (isIdle) {
+    return 'Submit a pokemon'
+  }
+
+  if (isRejected) {
+    throw state.error
+  }
+
+  if (isPending) {
+    return <PokemonInfoFallback name={pokemonName} />
+  }
+
+  if (isResolved) {
+    return <PokemonDataView pokemon={state.pokemon} />
+  }
+
+  throw new Error(`Unknown state: ${state}`)
+}
+
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>
+  )
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <ErrorBoundary key={pokemonName} FallbackComponent={ErrorFallback}>
+        <div className="pokemon-info">
+          <PokemonInfo pokemonName={pokemonName} />
+        </div>
+      </ErrorBoundary>
+    </div>
+  )
+}
+
+export default App
